bad8db36559f144b0ffdbb08d7f199497ec8b92d
Altimerra <altimerra@supernova>
2022-02-16 08:44:25 +0530

    general changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
-# TODO make this into a function
 # TODO refer to Config via env variable
 cd ~/Config
 for dir in *
 do
-    mkdir -p ~/.config/$dir
-    stow $dir -t ~/.config/$dir
+    case $dir in
+        Xresources)
+            ln -s ~/Config/Xresources ~/.Xresources
+            ;;
+        *)
+            mkdir -p ~/.config/$dir
+            stow $dir -t ~/.config/$dir
+            ;;
+    esac
 done
+
+systemctl --user enable --now emacs
+systemctl --user enable --now syncthing.service
+systemctl --user enable --now mpd.service
